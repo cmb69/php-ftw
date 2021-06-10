ce83b6f5fc2ab2a75dd296913a5979425d799ce6
Christoph M. Becker <cmbecker69@gmx.de>
2021-06-11 00:02:48 +0200

    checkout repo

--- .github/workflows/fetch-php.ps1
+++ .github/workflows/fetch-php.ps1
@@ -8,4 +8,4 @@ $ErrorActionPreference = "Stop"
 
 $url = "https://windows.php.net/downloads/releases/php-8.0.7-Win32-vs16-x64.zip"
 Invoke-WebRequest $url -OutFile "php-8.0.7-Win32-vs16-x64.zip"
-7z "x" "php-8.0.7-Win32-vs16-x64.zip" -ophp
+7z "x" "php-8.0.7-Win32-vs16-x64.zip" -ophpbin
