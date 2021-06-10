22551af858e6e0a06a17a2e8b86c8414071101d8
Christoph M. Becker <cmbecker69@gmx.de>
2021-06-11 01:37:52 +0200

    use args

--- .github/workflows/fetch-php.ps1
+++ .github/workflows/fetch-php.ps1
@@ -6,12 +6,14 @@ param (
 
 $ErrorActionPreference = "Stop"
 
-$fname = "php-8.0.7-nts-Win32-vs16-x64.zip"
+$tspart = if ($ts -eq "nts") {"nts-Win32"} else {"Win32"}
+
+$fname = "php-$version-$tspart-vs16-$arch.zip"
 $url = "https://windows.php.net/downloads/releases/$fname"
 Invoke-WebRequest $url -OutFile $fname
 7z "x" $fname "-ophpbin"
 
-$fname = "php-test-pack-8.0.7.zip"
+$fname = "php-test-pack-$version.zip"
 $url = "https://windows.php.net/downloads/releases/$fname"
 Invoke-WebRequest $url -OutFile $fname
 7z "x" $fname "-otests"
