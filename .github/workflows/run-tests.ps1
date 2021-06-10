fc2d13ceeeaf5b76206e80617f91a77ba5a254cf
Christoph M. Becker <cmbecker69@gmx.de>
2021-06-11 01:05:53 +0200

    avoid verbose output; export Junit .xml instead

--- .github/workflows/run-tests.ps1
+++ .github/workflows/run-tests.ps1
@@ -2,7 +2,8 @@ $ErrorActionPreference = "Stop"
 
 $Env:Path = "$pwd\phpbin;$Env:Path"
 $Env:TEST_PHP_EXECUTABLE = "$pwd\phpbin\php.exe"
+$Env:TEST_PHP_JUNIT = "$pwd\tests-results.xml"
 
 Set-Location "tests"
 
-php "run-tests.php" "-j4" "Zend\tests"
+php "run-tests.php" "-j4" "-g" "FAIL,XFAIL,XLEAK,SKIP,BORK,WARN,LEAK" "Zend\tests"
