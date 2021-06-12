d0135fd46b68d3fd069704003e24e222c95e59cc
Christoph M. Becker <cmbecker69@gmx.de>
2021-06-12 16:09:57 +0200

    qa

--- .github/workflows/fetch-php.ps1
+++ .github/workflows/fetch-php.ps1
@@ -6,14 +6,16 @@ param (
 
 $ErrorActionPreference = "Stop"
 
+$what = if ($version -match "[a-z]") {"releases"} else {"qa"}
+$baseurl = "https://windows.php.net/downloads/$what"
 $tspart = if ($ts -eq "nts") {"nts-Win32"} else {"Win32"}
 
 $fname = "php-$version-$tspart-vs16-$arch.zip"
-$url = "https://windows.php.net/downloads/releases/$fname"
+$url = "$baseurl/$fname"
 Invoke-WebRequest $url -OutFile $fname
 7z "x" $fname "-ophpbin"
 
 $fname = "php-test-pack-$version.zip"
-$url = "https://windows.php.net/downloads/releases/$fname"
+$url = "$baseurl/$fname"
 Invoke-WebRequest $url -OutFile $fname
 7z "x" $fname "-otests"
