67a9982d970e81c5bddc4de02c4d79a8052bd131
Christoph M. Becker <cmbecker69@gmx.de>
2021-06-30 12:19:31 +0200

    Support testing of PHP 7.3 and 7.4, too

--- .github/workflows/fetch-php.ps1
+++ .github/workflows/fetch-php.ps1
@@ -6,11 +6,23 @@ param (
 
 $ErrorActionPreference = "Stop"
 
+$versions = @{
+    "7.3" = "vc15"
+    "7.4" = "vc15"
+    "8.0" = "vs16"
+    "8.1" = "vs16"
+    "master" = "vs16"
+}
+$vs = $versions.$version
+if (-not $vs) {
+    throw "unsupported PHP version"
+}
+
 $what = if ($version -match "[a-z]") {"qa"} else {"releases"}
 $baseurl = "https://windows.php.net/downloads/$what"
 $tspart = if ($ts -eq "nts") {"nts-Win32"} else {"Win32"}
 
-$fname = "php-$version-$tspart-vs16-$arch.zip"
+$fname = "php-$version-$tspart-$vs-$arch.zip"
 $url = "$baseurl/$fname"
 Invoke-WebRequest $url -OutFile $fname
 7z "x" $fname "-ophpbin"
