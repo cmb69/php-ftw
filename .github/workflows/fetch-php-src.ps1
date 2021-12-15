c9d22139c0b07c306f4f2944ba7f2defdb209ddb
Christoph M. Becker <cmbecker69@gmx.de>
2021-12-15 16:00:23 +0100

    Support building a commit for ad-hoc snapshots

--- .github/workflows/fetch-php-src.ps1
+++ .github/workflows/fetch-php-src.ps1
@@ -5,7 +5,11 @@ param (
 
 $ErrorActionPreference = "Stop"
 
-Invoke-WebRequest "https://github.com/php/php-src/archive/refs/tags/php-$version.zip" -Outfile "php-$version.zip"
+if ($version.Contains(".")) {
+    Invoke-WebRequest "https://github.com/php/php-src/archive/refs/tags/php-$version.zip" -Outfile "php-$version.zip"
+} else {
+    Invoke-WebRequest "https://github.com/php/php-src/archive/$version.zip" -Outfile "php-$version.zip"
+}
 7z "x" "php-$version.zip"
 Move-Item "php-src-php-$version" "php-$version-src"
 7z "a" "artifacts\php-$version-src.zip" "php-$version-src"
