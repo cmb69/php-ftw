aba69a9e5b3031b9f61d4b5df9fca76835bc72ce
Christoph M. Becker <cmbecker69@gmx.de>
2022-05-01 14:29:14 +0200

    PECL/dbase

---
+++ .github/workflows/pecl.php
@@ -0,0 +1,29 @@
+<?php
+
+list($version, $arch, $ts, $extension) = array_slice($argv, 1);
+
+$versions = [
+    "7.4" => "vc15",
+    "8.0" => "vs16",
+    "8.1" => "vs16",
+];
+if (!array_key_exists($version, $versions)) {
+    throw new Exception("Unsupported version: $version");
+}
+echo "::set-output name=vs::{$versions[$version]}\n";
+
+$ini = parse_ini_file(__DIR__ . "/pecl.ini", true, INI_SCANNER_RAW);
+if (!array_key_exists($extension, $ini)) {
+    throw new Exception("Unsupported extension: $extension");
+}
+echo "::set-output name=config::{$ini[$extension]['config']}\n";
+
+$builddir = "";
+if ($arch === "x64") {
+    $builddir .= "x64\\";
+}
+$builddir .= "Release";
+if ($ts === "ts") {
+    $builddir .= "_TS";
+}
+echo "::set-output name=builddir::{$builddir}\n";
