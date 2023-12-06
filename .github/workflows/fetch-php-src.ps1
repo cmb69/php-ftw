c1381a4f6a601e9669ad046ed94cb35ced02daa4
Christoph M. Becker <cmbecker69@gmx.de>
2023-12-06 18:21:35 +0100

    fix

--- .github/workflows/fetch-php-src.ps1
+++ .github/workflows/fetch-php-src.ps1
@@ -11,6 +11,6 @@ if ($version.Contains(".")) {
     Invoke-WebRequest "https://github.com/php/php-src/archive/$version-clean.zip" -Outfile "php-$version.zip"
 }
 7z "x" "php-$version.zip"
-Move-Item "php-src-php-$version" "php-$version-src"
+Move-Item "php-src-php-$version-clean" "php-$version-src"
 7z "a" "artifacts\php-$version-src.zip" "php-$version-src"
 Move-Item "php-$version-src" "php/vs16/$arch/php-$version"
